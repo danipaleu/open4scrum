06b25ae7dadf0ee14d2487f8569d37b30655c01d
Andreas Ek <andreas@flowcom.se>
2013-02-27 00:08:34 +0100

    theme starter

--- wp/wp-content/themes/open4scrum/header.php
+++ wp/wp-content/themes/open4scrum/header.php
@@ -18,7 +18,7 @@
 	<meta name="author" content="Muhammad Usman">
 
 	<!-- The styles -->
-	<link id="bs-css" href="css/bootstrap-cerulean.css" rel="stylesheet">
+	<link id="bs-css" href="<?php echo get_stylesheet_directory_uri() ?>/css/bootstrap-cerulean.css" rel="stylesheet">
 	<style type="text/css">
 	  body {
 		padding-bottom: 40px;
@@ -27,22 +27,22 @@
 		padding: 9px 0;
 	  }
 	</style>
-	<link href="css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="css/charisma-app.css" rel="stylesheet">
-	<link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-	<link href='css/fullcalendar.css' rel='stylesheet'>
-	<link href='css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-	<link href='css/chosen.css' rel='stylesheet'>
-	<link href='css/uniform.default.css' rel='stylesheet'>
-	<link href='css/colorbox.css' rel='stylesheet'>
-	<link href='css/jquery.cleditor.css' rel='stylesheet'>
-	<link href='css/jquery.noty.css' rel='stylesheet'>
-	<link href='css/noty_theme_default.css' rel='stylesheet'>
-	<link href='css/elfinder.min.css' rel='stylesheet'>
-	<link href='css/elfinder.theme.css' rel='stylesheet'>
-	<link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
-	<link href='css/opa-icons.css' rel='stylesheet'>
-	<link href='css/uploadify.css' rel='stylesheet'>
+	<link href="<?php echo get_stylesheet_directory_uri() ?>/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="<?php echo get_stylesheet_directory_uri() ?>/css/charisma-app.css" rel="stylesheet">
+	<link href="<?php echo get_stylesheet_directory_uri() ?>/css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/fullcalendar.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/fullcalendar.print.css' rel='stylesheet'  media='print'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/chosen.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/uniform.default.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/colorbox.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/jquery.cleditor.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/jquery.noty.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/noty_theme_default.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/elfinder.min.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/elfinder.theme.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/jquery.iphone.toggle.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/opa-icons.css' rel='stylesheet'>
+	<link href='<?php echo get_stylesheet_directory_uri() ?>/css/uploadify.css' rel='stylesheet'>
 
 	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
@@ -50,7 +50,7 @@
 	<![endif]-->
 
 	<!-- The fav icon -->
-	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/img/favicon.ico">
 		
 </head>
 
@@ -65,7 +65,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</a>
-				<a class="brand" href="index.html"> <img alt="Charisma Logo" src="img/logo20.png" /> <span>Charisma</span></a>
+				<a class="brand" href="index.html"> <img alt="Charisma Logo" src="<?php echo get_stylesheet_directory_uri() ?>/img/logo20.png" /> <span>Charisma</span></a>
 				
 				<!-- theme selector starts -->
 				<div class="btn-group pull-right theme-container" >
