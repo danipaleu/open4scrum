fd1002f1f2d02448a4d49872c8dd27e550fcffb3
Andreas Ek <andreas@flowcom.se>
2013-02-27 16:04:34 +0100

    Analytics tracker

--- wp/wp-content/themes/open4scrum/header.php
+++ wp/wp-content/themes/open4scrum/header.php
@@ -29,6 +29,21 @@
 </head>
 
 <body>
+
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-10747578-7']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+
 	<?php if(!isset($no_visible_elements) || !$no_visible_elements)	{ ?>
 	<!-- topbar starts -->
 	<div class="navbar">
